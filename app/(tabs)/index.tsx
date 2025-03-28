85f9d99832f0a56eb4770b59bfa7ed1252486342
superman1408 <superman14082017@gmail.com>
2025-03-28 11:36:23 +0530

    changes made in index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ export default function HomeScreen() {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
       <Text className='text-red-500'>Home Screen</Text>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar />
     </View>
   );
 }
